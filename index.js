df1bcdcc4db3b90fa8165c6300dfefc4a2fe59e1
Alexandre de Paula <contatoaledepaula@gmail.com>
2023-11-02 09:00:33 -0300

    update cors endpoint vercel

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const port = 8000;
 
 const corsOptions = {
-    origin: '*',
+    origin: 'https://smartsendfrontend.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 };
 
